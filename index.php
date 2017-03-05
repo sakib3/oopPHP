560d0e1b6f4ac5dd07d4e03e12deec7d7826bedd
sakib3 <sakib3@gmail.com>
2017-03-05 14:09:13 +0100

    understanding how object communicate each other  or messaging

--- index.php
+++ index.php
@@ -21,24 +21,36 @@ class Business
   public function hire(Person $person){
     $this->staff->add($person);
   }
+
+  public function getStaffMembers(){
+    return $this->staff->members();
+  }
 }
 
 class Staff{
   protected $members = [];
 
+  function __construct($members = []){
+      $this->members = $members;
+  }
+
   public function add(Person $person){
     $this->members[] = $person;
+  }
+
+  public function members(){
+    return $this->members;
   }
 }
 
 
 $sabbir = new Person("Sabbir Rahman Sakib");
 
-$staff = new staff;
+$staff = new staff([$sabbir]);
 
 $teleopti = new Business($staff);
 
-$teleopti->hire($sabbir);
+$teleopti->hire(new Person("Sohali Safdar"));
 
 
-var_dump($staff);
+var_dump($teleopti->getStaffMembers());
